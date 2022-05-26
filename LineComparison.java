0d20c0b54423e4f22920d4e4ea11ff66559ff376
julinbestina <julin.bestina@yahoo.com>
2022-05-26 14:38:42 +0530

    Added functionality to check the equality of two lines

--- LineComparison.java
+++ LineComparison.java
@@ -1,14 +1,25 @@
 package com.bridgelabz.day3;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class LineComparison {
     public static void main(String[] args) {
 
-        //Scanner sc = new Scanner();
         int x1 = 1, y1 = 3, x2 =5, y2 = 3;
-        double lineLength = Math.sqrt((x2 - x1) ^ 2 + (y1 - y2) ^ 2);
-        System.out.println("Line Length= " + lineLength);
+        int x3 = -2, y3 = -1, x4 = 1, y4 = 3;
+
+        double line1Length = Math.sqrt((x2 - x1) ^ 2 + (y1 - y2) ^ 2);
+        double line2Length = Math.sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2);
+
+        System.out.println("Length of two lines : " + line1Length + ", " + line2Length);
+
+        boolean isEqual = Objects.equals(line1Length, line2Length);
+
+        if(isEqual)
+            System.out.println("Lines are Equal");
+        else
+            System.out.println("Lines are not Equal");
+
     }
 
 }
