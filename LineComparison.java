2f4beed70e938a93158da483c085473f5918a458
julinbestina <julin.bestina@yahoo.com>
2022-05-30 23:05:43 +0530

    Added code to Compare two lines

--- LineComparison.java
+++ LineComparison.java
@@ -1,24 +1,32 @@
 package com.bridgelabz.day3;
 
 import java.util.*;
+import java.lang.Integer;
 
 public class LineComparison {
     public static void main(String[] args) {
+        Integer x1 = new Integer(1);
+        Integer x2 = new Integer(5);
+        Integer x3 = new Integer(-2);
+        Integer x4 = new Integer(1);
+        Integer y1 = new Integer(3);
+        Integer y2 = new Integer(3);
+        Integer y3 = new Integer(-1);
+        Integer y4 = new Integer(3);
 
-        int x1 = 1, y1 = 3, x2 =5, y2 = 3;
-        int x3 = -2, y3 = -1, x4 = 1, y4 = 3;
-
-        double line1Length = Math.sqrt((x2 - x1) ^ 2 + (y1 - y2) ^ 2);
-        double line2Length = Math.sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2);
+        Double line1Length = Math.sqrt((x2 - x1) ^ 2 + (y1 - y2) ^ 2);
+        Double line2Length = Math.sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2);
 
         System.out.println("Length of two lines : " + line1Length + ", " + line2Length);
 
-        boolean isEqual = Objects.equals(line1Length, line2Length);
+        int isEqual = line1Length.compareTo(line2Length);
 
-        if(isEqual)
-            System.out.println("Lines are Equal");
+        if (isEqual > 0)
+            System.out.println("Line1 is greater than Line2 ");
+        else if (isEqual < 0)
+            System.out.println("Line2 is greater than Line1");
         else
-            System.out.println("Lines are not Equal");
+            System.out.println("Line1 is equal to Line2");
 
     }
 
